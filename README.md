805c30bb23534fca57ab369264411fe5e98258e3
Andy Yin <dev@fortheyin.com>
2017-05-23 19:16:14 -0400

    Initial commit of clean waterworks

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# clean-waterworks
-Cleans up WaterlooWorks to be more UI friendly
+# Clean WaterWorks
+Cleans up WaterlooWorks to be more UI friendly.
+
+![Demo Image](https://raw.githubusercontent.com/ForTheYin/clean-waterworks/master/demo.png)
+
+## Features
+* Moves the sidebar to the top navigation, saving precious horizontal space
+* Allow text in the job postings to wrap and not take up the entire screen
+* No more clicking on `View` button on the mailbox, just simply click on the message
+
+## Installation
+Install GreaseMonkey or TamperMonkey, and load the `cww.js` script.
---
+++ cww.js
@@ -0,0 +1,113 @@
+// ==UserScript==
+// @name         Clean WaterWorks
+// @namespace    http://fortheyin.com/
+// @version      0.0.1
+// @description  Cleans up WaterlooWorks to be more UI friendly
+// @author       Andy Yin
+// @match        https://waterlooworks.uwaterloo.ca/myAccount/*
+// @grant        none
+// ==/UserScript==
+
+(function () {
+  'use strict';
+
+  function sidebarToNav() {
+    // Gets all of the links, and names on the sidebar
+    function pruneSidebar() {
+      var links = [];
+      var $elements = $('.span2').find('li:has(a)');
+      $elements.each(function (i, $element) {
+        var $li = $element;
+        var $a = $li.children[0];
+
+        links.push({
+          href: $a.attributes.href.nodeValue,
+          name: $a.text,
+          active: ($li.attributes.class && $li.attributes.class.nodeValue === 'active') || false
+        });
+      });
+
+      return links;
+    }
+
+    // Adds links to a navbar
+    function addLinksToNav($nav, links) {
+      var $navbarInner = $('#brandingNav').find('.navbar-inner');
+      links.forEach(function (link) {
+        var liAttribute = link.active ? ' class="active"' : '';
+        var $li = $('<li' + liAttribute + '><a href="' + link.href + '">'+ link.name +'</a></li>');
+        $nav.append($li);
+      });
+
+      $navbarInner.append($nav);
+    }
+
+    // Create custom top navbar
+    var $nav = $('<ul class="nav navbar-nav"></ul>');
+    var links = pruneSidebar();
+
+    // Remove left sidebar
+    $('.span2').remove();
+    addLinksToNav($nav, links);
+
+    // Force content to take full width
+    $('.span10').css('width', '100%');
+  }
+
+  function normalizeJobPostings() {
+    var $postingsTable = $('#postingsTable');
+    if ($postingsTable.length === 0) {
+      return;
+    }
+
+    // Remove no-wrap on the job postings, so we can fit more content
+    $postingsTable.css('white-space', 'normal');
+    $postingsTable.find('td').css('white-space', 'normal');
+  }
+
+  function clickAnywhereMailbox() {
+    var $mailbox = $('#dashboard_userCommonMyMessagesTableID');
+    if ($mailbox.length === 0) {
+      return;
+    }
+
+    // Mail is asynchronously fetched, but not in consistent way, so if we're on
+    // the mailbox page, we need to check until the mail appears, and self-terminate
+    var interval = setInterval(function () {
+      var $headers = $mailbox.find('thead > tr');
+      var $messages = $mailbox.find('tbody > tr');
+
+      if ($headers.length === 0) {
+        return;
+      }
+      clearInterval(interval);
+
+      // Removes the view button
+      $headers[0].children[0].remove();
+
+      // Update all rows to be clickable
+      $messages.each(function (i, $message) {
+        var clickFunction = $message.children[0].children[0].onclick;
+        $message.onclick = clickFunction;
+        $message.children[0].remove();
+      });
+
+    }, 50);
+  }
+
+  sidebarToNav();
+  normalizeJobPostings();
+  clickAnywhereMailbox();
+
+  // Override load job postings table to renormalize the table after sort
+  var previousLoadPostingTable = window.loadPostingTable || function () {};
+  window.loadPostingTable = function (orderBy, oldOrderBy, sortDirection, page, searchBy, keyword, callback) {
+    previousLoadPostingTable(orderBy, oldOrderBy, sortDirection, page, searchBy, keyword, function () {
+      if (callback) {
+        callback();
+      }
+      normalizeJobPostings();
+    });
+  };
+
+})();
